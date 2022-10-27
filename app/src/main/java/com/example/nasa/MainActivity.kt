3af809c5ab96875115a005333b5bbb071cd18fe3
gennadyi90@gmail.com <Kutuzov1482003!>
2022-10-27 15:00:08 +0300

    1

---
+++ app/src/main/java/com/example/nasa/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.nasa
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.nasa.databinding.ActivityMainBinding
+import com.example.nasa.R
+import com.example.nasa.ui.main.PictureOfTheDayFragment
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, PictureOfTheDayFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/com/example/nasa/PictureOfTheDayAPI.kt
@@ -0,0 +1,13 @@
+package com.example.nasa
+
+
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface PictureOfTheDayAPI {
+    @GET("planetary/apod")
+    fun getPictureOfTheDay(@Query("api_key") apiKey: String):
+            Call<PODServerResponseData>
+}
+
